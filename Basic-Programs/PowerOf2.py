9402ba10b118f13ddf7da918987bc7c7d7369885
satyam4853 <vaishnavsatyam48532gmail.com>
2021-09-22 12:40:44 +0530

    [satyam] feat: Power of 2 Program

---
+++ Basic-Programs/PowerOf2.py
@@ -0,0 +1,15 @@
+"""
+   * Author - Satyam Vaishnav
+   * Date -  16-SEP-2021
+   * Time -  22.30 PM
+   * Title - Power of 2 table in range upto (1,31)
+"""
+
+for num in range(1,31):
+    print("table of power of 2 is =" , 2**num)
+    num += num
+
+
+    
+    
+    
